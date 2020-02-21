6d9990f9d069f2499cecde4526087ef0a3c7d183
kasambuli <cynthiakasambuli@gmail.com>
2020-02-21 19:12:06 +0300

    Feature(Add Contact): -Aded alert function for a new name entry

--- js/scripts.js
+++ js/scripts.js
@@ -7,38 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function findNames() {
 
-    var inputValue = document.getElementById('search').value;
+
+    var searchInputValue = document.getElementById('search').value;
 
     var contacts = document.getElementById('contacts')
     var names = contacts.querySelectorAll('li.collection-item')
 
     for (var i = 0; i < names.length; i += 1) {
-        if (names[i].innerHTML.toLowerCase().includes(inputValue)) {
+        if (names[i].innerHTML.toLowerCase().includes(searchInputValue)) {
 
             names[i].style.display = '';
         } else {
             names[i].style.display = 'none';
         };
-        //     elems[i].style.display = 'block';
-        // }
-        //     names.forEach(function(name) {
-        //         if (name.innerHTML.toLowerCase().includes(inputValue)) {
-        //             var theName = name.innerHTML
-        //             theName[0].style.display = '';
-        //         } else {
-
-        //         }
-        //     })
-
-        // for (i in names) {
-
-        //     var namees = names[i]
-        //     console.log(typeof namees.innerHTML)
-        //     if (namees.innerHTML.includes(inputValue)) {
-
-        //     } else {
-
-        //     }
-        // };
     };
+
 };
+
+function appendNewContact() {
+    var inputValue = document.getElementById('addContacts').value;
+    alert(inputValue)
+}
